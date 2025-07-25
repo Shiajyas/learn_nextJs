baeb9dc716ac399f2041b67a79e81b7e5031a4aa
shijayas <anjithashijay@gmail.com>
2025-07-25 20:07:50 +0530

    add not find for spacific route

--- hellow-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ hellow-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,13 @@
 
+import { notFound } from "next/navigation"
+
 export default async function({params}: {params: Promise<{productId: string,reviewId : string}>}){
     
     let {reviewId,productId} = await params
+
+    if(parseInt(reviewId) > 999){
+      notFound()
+    }
 
     return (
         <h1>product id {productId} and review id :{reviewId} </h1>
